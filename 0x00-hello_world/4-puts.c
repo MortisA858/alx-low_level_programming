d0d7080eb30fc84ff0444fa03d60480d8395ed2f
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-11 14:28:27 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main(void)
-/**
- * main - Entry point
- */
+/* main - Entry point*/
+
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle"\n);
 	return(0);
 }
