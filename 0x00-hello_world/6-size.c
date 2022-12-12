b3426b4f17c518ba6d32d08af7000da01f777ad5
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-12 01:52:03 -0800

    The eggs of the brood need to be an odd number

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ int main(void)
 	long long int c;
 	char d;
 	float e;
-
 	printf("Size of char: %lu byte(s)\n",(unsigned long)sizeof(d));
 	printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of long int: %lu byte(s)\n", (unsigned long)sizeof(b));
