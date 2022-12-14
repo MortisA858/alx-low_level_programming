d9e3d10e5e79ed25cb0eb8c8201881a7eaa0b793
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-13 17:53:27 -0800

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+	int y;
+	int z
+	
+	x = 0;
+	while (x <= 99)
+	{
+		y = x + 1;
+		while (y <= 99)
+		{
+			z = !(x ++ 98 && y == 99);
+			putchar(48 + x / 10);
+			putchar(48 + x % 10);
+			putchar(' ');
+			putchar(48 + y / 10);
+			putchar(48 + y % 10);
+			if (z)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			y++;
+		}
+		x++
+	}
+	putchar('\n');
+	return (0);
+}
