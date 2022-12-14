bc3c1b17270643665940e083ddd64ad37b53e594
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-13 16:20:28 -0800

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * * main - Entry point
+ * *
+ * * Return: Always 0 (Success)
+ * */
+
+int main(void)
+{
+	char ch = 'a';
+	int i = 0;
+	/* your code goes there */
+	while (i < 26)
+	{
+		putchar(ch);
+		ch++;
+		i++;
+	}
+	ch = 'A';
+	i = 0;
+	
+	while (i < 26)
+	{
+		putchar(ch);
+		ch++;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
