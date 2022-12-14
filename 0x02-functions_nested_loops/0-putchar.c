04dbb686704013b881599b87eaad3ec9fced8cee
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-14 14:45:56 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_HEADER_
+#define MAIN_HEADER_
+
+/**
+ * function - _putchar stored in _putchar.c
+ *
+ * 
+ */ 
+
+int _putchar(void)
+
+#endif
