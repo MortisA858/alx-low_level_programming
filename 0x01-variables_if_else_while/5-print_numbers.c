37e72be4725c7163f6c696aa930974c6d48ba1d4
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-13 16:48:28 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)   
+*/
+
+int main(void)
+{
+	char ch = '0';
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(ch);
+		ch++;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
