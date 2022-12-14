fbd864adb831b6138ce47e240d73e2e2c77aadb6
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-13 17:08:22 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	int i = 48;
+
+	while (i< 58)
+	{
+		putchar(i);
+		i++;
+	}
+	i = 0;
+	while (i < 6)
+	{
+		putchar(ch);
+		ch++;
+		i++
+	}
+	putchar('\n');
+	return (0)
+}
