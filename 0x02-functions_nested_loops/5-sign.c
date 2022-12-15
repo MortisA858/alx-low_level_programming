695019d90448f41a93d709bcb05f43d38d47d734
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-15 15:38:06 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,15 +9,19 @@
  */
 int print_sign(int n)
 {
-	if (n > 0){
-		_putchar(+);
+	if (n > 0)
+	{
+		_putchar('+');
 		return (1);
 	}
-	else if (n < 0){
-		_putchar(-);
+	else if (n < 0)
+	{
+		_putchar('-');
 		return (-1)
 	}
 	else
-		_putchar(0);
+	{
+		_putchar('0');
 		return (0);
+	}
 }
