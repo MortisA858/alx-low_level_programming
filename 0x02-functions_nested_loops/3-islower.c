adb6cdbbb83a862a25d0a639b4bfa1ea5517404e
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-15 14:32:46 -0800

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _islower(int c) - checks for lowercase character.
+ *
+ * Author: AlgoSierra
+ */
+
+int _islower(int c)
+{
+	if (_islower = c)
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
