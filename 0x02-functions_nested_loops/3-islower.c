9725c8b8ec3310ebecfde4bb37a7028786718de4
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-15 14:57:27 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,11 +4,13 @@
  * int _islower(int c) - checks for lowercase character.
  *
  * Author: AlgoSierra
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
  */
 
 int _islower(int c)
 {
-	if (_islower = c)
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
--- 0x02-functions_nested_loops/3-main.c
+++ 0x02-functions_nested_loops/3-main.c
@@ -1,10 +1,11 @@
-#include <main.h>
+#include "main.h"
 
 /**
  * main - check the code.
  *
  * Return: Always 0.
  */
+int main(void)
 {
 	int r;
 
