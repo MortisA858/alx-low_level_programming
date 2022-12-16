d033ff43f8acbb6fa0af552cf0e6f6d255ae7933
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-16 04:57:33 -0800

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,12 +13,15 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-        {
-		putchar(i);
-    	}
+	/**
+	 * while (i < 10)
+	 * {
+	 * Infinite loop - No increase of variable
+	 * putchar(i);
+	 * }
+	 */
 
 	printf("Infinite loop avoided! \\o/\n");
 
         return (0);
-} /* Prints infinite loop */
+}
