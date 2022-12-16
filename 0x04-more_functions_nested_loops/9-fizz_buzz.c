c61e8c83852409435c25ff25047a114a0b6c5ba1
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-16 15:56:43 -0800

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: print the number 1-100.
+ * Fizz multiple of 3, Buzz multiple of 5
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; 1 <= 100; i++)
+	{
+		if (i % 15 == 0)
+		{
+		printf("FizzBuzz");
+		}
+		else if (i % 3 == 0);
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%i", i);
+		}
+		if (i < 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
