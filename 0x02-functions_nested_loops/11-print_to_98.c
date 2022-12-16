e94f2513aef2b8d6b74ea9a229edb1855c3e819b
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-15 17:31:33 -0800

    98 Battery Street, the OG mandatory

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * print_to_98 - function that prints all natural numbers from n to 98, followed by a new line.
+ * print_to_98 - function that prints natural numbers from n to 98, adds new line.
  * @n: First that begins the count
  */
 void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for  (n = n; n <98; n++)
+		for  (n = n; n < 98; n++)
 			printf("%d, ", n);
 		printf("%d\n", n);
 	}
