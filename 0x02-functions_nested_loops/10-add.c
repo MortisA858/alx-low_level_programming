92dc05944642707843d3baf924be148389a1db1e
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-15 17:09:35 -0800

    a+b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,4 +1,19 @@
 #include "main.h"
 
 /**
+ * add - function that adds two integers and return the results
  *
+ * Return: 0
+ */
+int add(int, int)
+{
+	int i;
+	int j;
+	int sum;
+
+	while ( sum = i + j)
+	{
+		_putchar(sum)
+	}
+	_putchar('\n')
+}
