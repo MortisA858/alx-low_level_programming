6c8b8d59df5f81647938debf08cd86e45fec582b
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-15 18:30:57 -0800

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,14 +9,14 @@ int main(void)
 
 	x = sum = 0;
 	y = 1;
-	for (count -0; count < 50; count ++)
+	for (count = 0; count < 50; count ++)
 	{
 		z = x + y;
 		x = y;
 		y = z;
 		if (z % 2 == 0 && z < 4000000)
-			sum++ k;
+			sum++ z;
 	}
 	printf("%lu\n", sum);
-	return;
+	return (0);
 }
