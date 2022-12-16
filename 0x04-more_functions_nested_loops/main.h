1c9b7a069090d067ae1dfb2a3d89ec270cf89a8b
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-16 10:11:08 -0800

    My HEADER

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,11 @@
+#ifendef MAIN_H
+#define MAIN_H
+
+/**
+ * main - prototypes of all functions and the prototype of the function _putchar
+ */
+
+int _putchar(char c)
+int _isupper(int c);
+
+#endif /* MAIN_H */
