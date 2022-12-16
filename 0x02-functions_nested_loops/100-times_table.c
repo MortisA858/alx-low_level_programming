f6ecfa3cd81508aa57513d1a16ab33a39cc25cec
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-15 17:52:07 -0800

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * print_times_table - prints the times table for n.
+ * @n: The multiplication table requested.
+ * Return: 0
+ */
+void print-times_table(int n)
+{
+	int i, j, result;
+
+	if (!(n > 15 || n < 0))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j<= n; j++)
+			{
+			result = (i * j);
+			if ( j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (result < 10 && j != 0)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar((result % 10) + '0');
+			}
+			else if (result >= 10 && result < 100)
+			{
+				_putchat(' ');
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			else if (result >= 100 && j != 0)
+			{
+				_putchar((result / 100) + '0');
+				_putchar((result / 10) % 10 + '0');
+				_putchar((result % 10) + '0');
+			}
+			else
+				_putchar((result % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
