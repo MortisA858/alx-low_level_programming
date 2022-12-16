3c75c30abeff558e99a56865c3f2c50dc9fbe391
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-15 16:39:56 -0800

    I'm federal agent Jack Bauer, and today is the longest day of my life mandatory

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,7 +4,7 @@
  * jack_bauer - function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
  * Return: 0
  */
-void jack_bauer(void);
+void jack_bauer(void)
 {
 	int hrs = 0;
 	int min = 0;
