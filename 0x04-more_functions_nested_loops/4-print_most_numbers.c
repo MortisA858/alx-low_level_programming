0f5876a02120799525349e0265aa934d5e4408c5
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-16 14:14:37 -0800

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,19 +9,14 @@
  */
 void print_most_number(void)
 {
-	int i = 0;
+	int i;
 
-	for (; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i == 2 || i == 4)
+		if (!(i == 2 || i == 4))
 		{
-			continue;
-		}
-		else
-		{
-			_putchar(i + '0');
+			_putchar(i);
 		}
 	}
-
 	_putchar('\n');
 }
