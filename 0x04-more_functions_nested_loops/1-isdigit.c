b3d4bda23b698e5c91e9e06e1aec7c18984bcd27
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-16 10:56:45 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that check for a digit 0 through 9.
+ * @c: checked digit
+ * Return: 1 if c is a digit 0 otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
