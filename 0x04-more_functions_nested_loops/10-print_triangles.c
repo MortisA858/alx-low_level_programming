849cd8a02376a2a93f35df340fbc4912f4a0a26b
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-16 16:24:37 -0800

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangles.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - function that prints a triangle
+ * Description: prints a triangle through diagonal form
+ * @size: it's the size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+ 	int row, hash, space;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+		for (space = size - row; space >= 1; space--)
+		{
+			_putchar(' ');
+		}
+		for (hash = 1; hash <= row; hash++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n);
+		}
+	}
+}
