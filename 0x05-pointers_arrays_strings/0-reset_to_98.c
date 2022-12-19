100dafebfcad11c23048b1a4e8cd6c569dd5eb54
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-19 15:00:23 -0800

    I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer to an int as parameter 
+ * and updates the value it points to 98
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	int i;
+	int *n;
+	
+	i = 98;
+	n = &i;
+	_putchar("i=%d\n, ");
+	reset_to _98(&i);
+	_putchar('i');
+}
