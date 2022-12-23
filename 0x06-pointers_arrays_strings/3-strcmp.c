413f00d99b9752d34dbdd0a7eb1669875350ba68
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-23 04:09:38 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,27 +1,26 @@
 #include "main.h"
 
 /*
- * _strcmp - compares two strings
- * s1: string
- * s2: strmp
+ * main - Entry point
+ * _strcmp - compares string values
+ * s1: input value
+ * s2: input value
  *
  * Return: integer
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		s1++;
-		s2++;
+		if (s1[i] != s2[i])
+		{
+			return (0);
+		}
+		i++;
 	}
 
-	if (*s1 == *s2)
-	{
-		return (0);
-	}
-	else
-	{
-		return (*s1 - *s2);
-	}
+	return (0);
 }
