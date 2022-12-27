4cccfe66524043c9a6a1a6b8a45153665c566dd3
AlgoSierra <nangiroevanstioko@gmail.com>
2022-12-27 04:50:25 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -8,18 +8,23 @@
  */
 char *rot13(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0')
 	{
-		if (s[i] >= 'A' && s[i] <= 'Z')
-	{
-		s[i] = (s[i] - 'A' + 13) % 26 + 'A';
-	}
-		else if (s[i] >= 'a' && s[i] <= 'z')
-	{
-		s[i] = (s[i] - 'a' + 13) % 26 + 'a';
-	}
+		while ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))
+		{
+			if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+			{
+				s[i] += 13;
+			}
+			else
+			{
+				s[i] -= 13;
+			}
+			i++;
+		}
+		i++;
 	}
 	return (s);
 }
