f8bea13bd17a6c3eb0ffbefe625d514c904b78a1
AlgoSierra <nangiroevanstioko@gmail.com>
2023-01-06 05:39:50 -0800

    Inception. Now, before you bother telling me it's impossible...

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * is_palindrome - function that returns 1 if string is palindrome and 0 if not
+ * @s: string
+ * 
+ * Return: 1 if a string is a palindrome and 0 if not.
+ */
+int is_palindrome(char *s)
+{
+	char s;
+
+	if (s = " ")
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
