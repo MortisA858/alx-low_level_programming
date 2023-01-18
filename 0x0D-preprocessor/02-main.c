1d0bd5e71b07585b8fba0cc5f996283fedd77d48
AlgoSierra <nangiroevanstioko@gmail.com>
2023-01-18 10:48:39 -0800

    File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints name of the file of the program
+ * it was compiled from, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
