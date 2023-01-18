02049ac708f572de39a78b967b13d6d1745ab368
AlgoSierra <nangiroevanstioko@gmail.com>
2023-01-18 10:55:38 -0800

    Function-like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,12 +1,13 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
+ * function-like macro ABS(x) that computes the absolute value of a number x
  * define - define macro
  * @ABS: macro
  * @x: value
  */
 
-#define ABS(x) ((x) < 0 ? ((x) * (-1)) : (x))
+#define ABS(x) ((x) < 0 ? ((x) * (-1)) : (x)i)
 
 #endif
