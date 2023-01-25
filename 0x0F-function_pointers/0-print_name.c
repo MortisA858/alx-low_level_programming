9f13f8a62afcd02827c3a2f9f2f5bcfab7080dce
AlgoSierra <nangiroevanstioko@gmail.com>
2023-01-25 08:43:29 -0800

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * @f: function pointer
+ *
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
