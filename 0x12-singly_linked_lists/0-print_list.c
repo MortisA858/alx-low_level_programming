5e3ba8eaaeec90002c8588129cd975b582aad4ad
AlgoSierra <nangiroevanstioko@gmail.com>
2023-02-01 10:37:47 -0800

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,17 +16,17 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-	if (!h->str)
-	{
-		printf("[0] (nil)\n");
-	}
-	else
-	{
-		printf("[%u] %s\n", h->len, h->str);
-	}
+		if (h->str)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
 
-	h = h->next;
-	count++;
+		count++;
+		h = h->next;
 	}
 
 	return (count);
