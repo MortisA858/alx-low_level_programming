6e6fe150243b791519d2c55df5706ecfcb63f6b2
AlgoSierra <nangiroevanstioko@gmail.com>
2023-02-01 05:47:23 -0800

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - function that frees a list_t list
+ * @head: double pointer to head of list
+ *
+ * Return: void.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp; /*Declare a pointer to a list_t structure called temp */
+
+	while (head)/*While the head of the list is not NULL*/
+	{
+		temp = head->next;
+		free(temp);
+		head = temp;
+	}
+}
