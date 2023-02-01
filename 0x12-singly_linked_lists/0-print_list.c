efa7b70da23a50f6269bc134249b414b6bff95d5
AlgoSierra <nangiroevanstioko@gmail.com>
2023-02-01 10:04:08 -0800

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,10 +13,10 @@ size_t print_list(const list_t *h)
 {
 	/*Initialize a counter for the number of nodes */
 	size_t count = 0;
-
+	
 	while (h)
 	{
-		if (!h->str)
+	if (!h->str)
 		{
 			printf("[0] (nil)\n");
 		}
