5ee14c0c8c1925e3b358eca7f8ae7dba262bf3fa
AlgoSierra <nangiroevanstioko@gmail.com>
2023-02-01 10:18:58 -0800

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -16,22 +16,13 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 /**
  * typdef struct list_s - defines a custom data type 'list_t' using a structure
  * const list_t *h - a pointer to a constant list_t structure,
  * representing the head of the linked list.
  */
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-
-int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
