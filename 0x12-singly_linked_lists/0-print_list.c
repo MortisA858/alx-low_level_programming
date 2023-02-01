0229ebefbad3a173de2c41ec46a8989b915d63e3
AlgoSierra <nangiroevanstioko@gmail.com>
2023-02-01 02:14:30 -0800

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,26 +11,32 @@
 size_t print_list(const list_t *h)
 {
 	/* Variable count stores number of nodes and its value is set to 0. */
-	size_t count = 1;
+	size_t count = 0;
 	const list_t *temp = h;
 
+	if (temp == NULL)
+		return (0);
+
 	/* traverse through the linked list */
-	while (temp)
+	while (temp != NULL)
 	{
 		/* If 'str' member is 'NULL', print [0] (nil) */
 	       /* If 'str' member is not 'NULL', print [h->len] h-> str */
 		if (temp->str == NULL)
 		{
-			printf("[%d] (nill)\n", temp->len);
+			printf("[%d] %s\n", 0, ("nil"));
 		}
 		else
 		{
 			printf("[%d] %s\n", temp->len, temp->str);
 		}
-
+		/* Move to the next node in the linked list */
 		temp = temp->next;
+		/* Increment the number of nodes */
 		count++;
 	}
+	/*Print the last node*/
+	printf("[%d] %s\n", h->len, h->str);
 
 	return (count);
 }
