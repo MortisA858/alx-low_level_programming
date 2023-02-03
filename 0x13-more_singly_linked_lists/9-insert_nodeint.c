54c0c58c91a6827b7331ec2d3e79ef04fbd292e8
AlgoSierra <nangiroevanstioko@gmail.com>
2023-02-03 11:04:09 -0800

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,20 +14,30 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 	listint_t *node, *new_node;
 
-	node = *head;
-	for (i = 0; i < idx - 1 && node != NULL; i++)
-		node = node->next;
-	if (node == NULL)
+	if (head == NULL)
 		return (NULL);
-
+	if (idx != 0)
+	{
+		node = *head;
+		for (i = 0; i < idx - 1 && node != NULL; i++)
+		{
+			node = node->next;
+		}
+		if (node == NULL)
+			return (NULL);
+	}
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	new_node->next = node->next;
 	node->next = new_node;
-
 	return (new_node);
 }
 
