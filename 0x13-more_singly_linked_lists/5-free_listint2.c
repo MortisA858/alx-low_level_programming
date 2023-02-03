262fbc9c7d689b5344c4fcc52a933d3a97ad1458
AlgoSierra <nangiroevanstioko@gmail.com>
2023-02-03 04:46:29 -0800

    Free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list. Sets head to NULL
+ * @head: double pointer to head of list
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *new_node, *temp;
+	temp = *head;
+
+	while (temp)
+	{
+		new_node = temp->next;
+		free(temp);
+		temp = new_node;
+	}
+
+	*head = NULL;
+}
